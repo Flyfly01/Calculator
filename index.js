6490d9a57d900fbd3504e924baf3397accdd1092
Flyfly01 <shannonimbert22@gmail.com>
2025-03-13 14:38:23 -0400

    Create operator functions

---
+++ index.js
@@ -0,0 +1,7 @@
+let a = null;
+let b = null;
+
+const add = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+const multiply = (a,b) => a * b;
+const divide = (a,b) => ((b === 0) ? "Are you dumb?" : a /b);
