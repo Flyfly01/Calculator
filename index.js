dd651f06ccc03c5847a1aa1b666e22688561742d
Flyfly01 <shannonimbert22@gmail.com>
2025-03-13 14:45:03 -0400

    Create operate func

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ const add = (a,b) => a + b;
 const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => ((b === 0) ? "Are you dumb?" : a /b);
+
+function operate(a, b, op) {
+  switch (op) {
+    case "+":
+      return add(a,b);
+    case "-":
+      return subtract(a,b);
+    case "*":
+      return multiply(a,b);
+    case "/":
+      return divide(a,b);
+    default:
+      return "Invalid operator";
+  }
+}
