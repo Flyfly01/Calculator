d3c41777d49ad56b19c3088b0238141b58fee5a9
Flyfly01 <shannonimbert22@gmail.com>
2025-03-14 19:37:21 -0400

    do logic for number click and operator click

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let a = null;
 let b = null;
+let firstNum = "";
+let op = "";
 
 const add = (a,b) => a + b;
 const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => ((b === 0) ? "Are you dumb?" : a /b);
+
+const numberButtons = document.querySelectorAll(".number");
+const displayElement = document.querySelector(".display p");
+const operatorButtons = document.querySelector(".operator");
 
 function operate(a, b, op) {
   switch (op) {
@@ -19,4 +25,27 @@ function operate(a, b, op) {
     default:
       return "Invalid operator";
   }
+}
+
+numberButtons.forEach( (button) => {
+  button.addEventListener("click", handleNumberClick);
+})
+
+function handleNumberClick (event) {
+  const number = event.target.textContent;
+  updateDisplay(number);
+}
+
+function updateDisplay(value) {
+  displayElement.textContent += value;
+}
+
+operatorButtons.forEach ( (button) => {
+  button.addEventListener("click", handleOperatorClick);
+})
+
+function handleOperatorClick (event) {
+  firstNum = displayElement.textContent;
+  op = event.target.textContent;
+  displayElement.textContent = ""; 
 }
